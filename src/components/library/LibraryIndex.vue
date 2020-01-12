ec9f585ed3bfb526fc46b40e446e1c2f444a9b3b
Aomrsou <1522017075@qq.com>
2020-01-12 19:41:24 +0800

    前端框架

---
+++ src/components/library/LibraryIndex.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-aside style="width: 200px;margin-top: 20px">
+      <switch></switch>
+      <SideMenu></SideMenu>
+    </el-aside>
+    <el-main>
+      <!--<books></books>-->
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import SideMenu from './SideMenu'
+export default {
+  name: 'AppLibrary',
+  components: {SideMenu}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 导入刚才编写的组件
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+import Home from '../components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    // 下面都是固定的写法
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: 'home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
+    {
+      path: '/',
+      redirect: '/login'
+    }
+  ]
+})
