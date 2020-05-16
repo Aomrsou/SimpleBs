81268c949552cf08ac36cd37b5f540f57620dbd2
Aomrsou <1522017075@qq.com>
2020-05-16 19:22:14 +0800

    导出excel以及fixbug

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import JsonExcel from 'vue-json-excel'
 // 设置反向代理，前端请求默认发送到 http://localhost:8888/bs
 var axios = require('axios')
-// axios.defaults.baseURL = 'http://39.105.72.22:8888/bs'
-axios.defaults.baseURL = 'http://localhost:8888/bs'
+axios.defaults.baseURL = 'http://39.105.72.22:8888/bs'
+// axios.defaults.baseURL = 'http://localhost:8888/bs'
 // 全局注册 ，之后可在其他组件中通过 this.$axios 发送数据
 // docker build -t zzy-vue:V1.0.0 .
 // docker run -p 80:80 -d --name zzybs imageId
